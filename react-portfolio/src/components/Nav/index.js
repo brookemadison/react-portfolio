31b427c0ef088202e5f2fe071d9c94c47984afa9
brookemadison <brookeperry611@gmail.com>
2021-11-04 14:02:04 -0400

    updated the links and title on hamburger menu

--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, Nav, Offcanvas, } from "react-bootstrap";
 
 // function Nav() {
 
@@ -42,31 +42,31 @@ import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 const Navigation = (props) => {
   return(
 
-<Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-  <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
+<Navbar bg="light" expand={false}>
+  <Container fluid>
+    <Navbar.Brand href="#">Brooke Madison</Navbar.Brand>
+    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+    <Navbar.Offcanvas
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+      placement="end"
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title id="offcanvasNavbarLabel">Brooke Madison</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav.Link href="#action1">Home</Nav.Link>
+          <Nav.Link href="#action2">About</Nav.Link>
+          <Nav.Link href="#action3">Projects</Nav.Link>
+          <Nav.Link href="#action4">Contact</Nav.Link>
+        </Nav>
+       
+      </Offcanvas.Body>
+    </Navbar.Offcanvas>
   </Container>
 </Navbar>
+
    )
   }
 
