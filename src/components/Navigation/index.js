2f6763608443013ffc5b4068c278cfb7ee3b7cf0
brookemadison <brookeperry611@gmail.com>
2021-11-12 18:00:36 -0500

    not deploying to github properly

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -9,7 +9,7 @@ const Navigation = () => {
         <Navbar.Brand>
         <NavLink 
         className="navbar-brand"
-                to='/home'>
+                to='/react-portfolio'>
                   Brooke Madison
             </NavLink>
         </Navbar.Brand>
@@ -34,7 +34,7 @@ const Navigation = () => {
             <Nav className="justify-content-end flex-grow-1 pe-3 navbar-offcanvas">
               <Nav.Link>
             <NavLink className="navbar-offcanvas"
-                style={{ color: "#f5efea" }} to='/home'>
+                style={{ color: "#f5efea" }} to='/react-portfolio'>
             Home
             </NavLink>
               </Nav.Link>
