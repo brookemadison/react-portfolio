99cc083a808f91a4df59e11476727d246f35e3c0
brookemadison <brookeperry611@gmail.com>
2021-11-12 09:29:26 -0500

    added descriptions and more styles to project section

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -26,14 +26,14 @@ import BudgetTracker from "../../assets/projects/3.png";
 
 const Project = (props) => {
   return (
-<Row xs={1} md={2} className="g-4">
+<Row xs={1} md={2} className="g-4 project-section">
   {Array.from({ length: 1 }).map((_, idx) => (
     
     <Col >
       <Card>
         <Card.Img variant="top" src={ProjectThree} />
         <Card.Body>
-          <Card.Title>Project Three</Card.Title>
+          <Card.Title><h4>Project Three</h4></Card.Title>
           <Card.Text>
             Details coming soon
           </Card.Text>
@@ -48,9 +48,11 @@ const Project = (props) => {
       <Card>
         <Card.Img variant="top" src={ProjectTwo} />
         <Card.Body>
-          <Card.Title>Project Two</Card.Title>
+          <Card.Title><h4>Project Two - Pokédex</h4></Card.Title>
           <Card.Text>
-            Details coming soon
+          <p>An online Pokédex that allows users to view and search for their favorite original 151 Pokémon.</p>
+          <p>Technologies Used:</p>
+          <p>HTML / CSS / Javascript / Node.js / Express / MySQL / dotenv / Heroku / jawsDB /Sequelize / Bcrypt / express-session / Handlebars / Animate.css / PokéAPI</p>
           </Card.Text>
           <Button className="projects-btns"  variant="outline-dark">See Deployed</Button>
           <Button className="projects-btns"  variant="outline-dark">View on Github</Button>
@@ -61,9 +63,11 @@ const Project = (props) => {
       <Card>
         <Card.Img variant="top" src={ProjectOne} />
         <Card.Body>
-          <Card.Title>Project One</Card.Title>
+          <Card.Title><h4>Project One - Decide Eat</h4></Card.Title>
           <Card.Text>
-            Details coming soon
+            <p>A date night app crafted to offer a unique experience to users by taking a quiz that pairs you with a recipe, cocktail, and music playlist recommendation</p>
+            <p>Technologies Used:</p>
+            <p>HTML / CSS / Javascript / jQuery / Bulma / Spoonacular API / Spotify API</p>
           </Card.Text>
           <Button className="projects-btns"  variant="outline-dark">See Deployed</Button>
           <Button className="projects-btns"  variant="outline-dark">View on Github</Button>
@@ -74,9 +78,11 @@ const Project = (props) => {
       <Card>
         <Card.Img variant="top" src={BudgetTracker} />
         <Card.Body>
-          <Card.Title>BudgetTracker</Card.Title>
+          <Card.Title><h4>BudgetTracker</h4></Card.Title>
           <Card.Text>
-            Details coming soon
+            <p>A PWA budget tracker application allows for offline access and functionality to to add expenses and deposits to their budget with or without a connection.</p>
+            <p>Technologies Used:</p>
+            <p>Javascript / indexedDB / Express / MongoDB / Mongoose / Heroku</p>
           </Card.Text>
           <Button className="projects-btns"  variant="outline-dark">See Deployed</Button>
           <Button className="projects-btns"  variant="outline-dark">View on Github</Button>
@@ -89,11 +95,3 @@ const Project = (props) => {
 
 
 export default Project;
-
-
-// const Project = ()=>{
-//   return(
-
-//       <div id="Project">
-//        <h1>Projects</h1>
-//         </div>
