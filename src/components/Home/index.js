b1c0e7a86d6b28e1aa9a64f49644a0d04b1103fe
brookemadison <brookeperry611@gmail.com>
2021-12-04 17:15:49 -0500

    cleaned up code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import Typewriter from 'typewriter-effect';
+import React from "react";
+import Typewriter from "typewriter-effect";
 
-const Home = ()=>{
-    
-  return(
-    <div className='home-section'>
-    <Typewriter
-      options={{
-        strings: ['Hi, I\'m Brooke.', 'Welcome to my portfolio.', 'Please look around!'],
-        autoStart: true,
-        loop: true,
-      }}
-    />
+const Home = () => {
+  return (
+    <div className="home-section">
+      <Typewriter
+        options={{
+          strings: [
+            "Hi, I'm Brooke.",
+            "Welcome to my portfolio.",
+            "Please look around!",
+          ],
+          autoStart: true,
+          loop: true,
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- src/components/NotFoundPage/index.js
+++ src/components/NotFoundPage/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const NotFoundPage = () => {
   return (
-  <section className="not-found-page">
-  <h2>Uh Oh.</h2>
-  <p>No page found. Sorry about that! Let's get you back to where you belong.</p>
-  <div className='btns'>
-            <a href='/react-portfolio'>
-              Go Back
-            </a>
-          </div>
-  </section>
-
+    <section className="not-found-page">
+      <h2>Uh Oh.</h2>
+      <p>
+        No page found. Sorry about that! Let's get you back to where you belong.
+      </p>
+      <div className="btns">
+        <a href="/react-portfolio">Go Back</a>
+      </div>
+    </section>
   );
 };
 
