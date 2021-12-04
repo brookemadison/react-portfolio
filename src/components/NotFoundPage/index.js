6d3d4496e3bcdcd50bec9e8713a6ce55cf2b3d54
brookemadison <brookeperry611@gmail.com>
2021-12-04 01:09:55 -0500

    added link to not found page

--- src/components/NotFoundPage/index.js
+++ src/components/NotFoundPage/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const NotFoundPage = () => {
-  return <h2>404 Page NotFound</h2>;
-}
+  return (
+  <section className="not-found-page">
+  <h2>Uh Oh.</h2>
+  <p>No page found. Sorry about that.</p>
+  <div className='btns'>
+            <a href='/react-portfolio'>
+              Contact Me
+            </a>
+          </div>
+  </section>
+
+  );
+};
 
 export default NotFoundPage;
