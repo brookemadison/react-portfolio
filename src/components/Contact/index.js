81559f0178bc3242f7fc416315f46222f282bef6
brookemadison <brookeperry611@gmail.com>
2022-01-03 18:01:21 -0500

    changed fetch url

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -13,7 +13,7 @@ const ContactForm = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://www.brookemadison.tech/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
