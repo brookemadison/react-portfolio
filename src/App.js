70dc7a1fc31f571d2d35bb176e5e38cb3e45c582
brookemadison <brookeperry611@gmail.com>
2022-03-22 22:19:54 -0400

    added cname file

--- src/App.js
+++ src/App.js
@@ -15,16 +15,6 @@ function App() {
   return (
     <BrowserRouter>
     <div>
-<<<<<<< HEAD
-    <Nav/>
-      <Switch>
-        <Route component={Home} path='/https://www.brookemadison.tech/' exact />
-        <Route component={About} path='/https://www.brookemadison.tech/about'/>
-        <Route component={Project} path='/https://www.brookemadison.tech/project'/>
-        <Route component={Resume} path='/https://www.brookemadison.tech/resume'/>
-        <Route component={Contact} path='/https://www.brookemadison.tech/contact'/>
-      </Switch>
-=======
       <Nav/>
       <Routes>
         <Route path='https://www.brookemadison.tech' element={<Home />} />
@@ -34,7 +24,6 @@ function App() {
         <Route path='contact' element={<Contact />} />
         <Route path='notfoundpage' element={<NotFoundPage />} />
       </Routes>
->>>>>>> 51573a20e7b746b36ac9ceeb3f1d7a38b5969146
       <Footer/>
     </div>
     </BrowserRouter>
